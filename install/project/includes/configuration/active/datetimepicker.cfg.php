44079b65f1c2c964a8bbaa42fb90477b3cee5a33
kukrik <tiit.papp@gmail.com>
2020-10-09 00:05:15 +0300

    Initial initialization

---
+++ install/project/includes/configuration/active/datetimepicker.cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * datetimepicker defines
+ */
+
+define('QCUBED_DATETIMEPICKER_URL', dirname(QCUBED_PROJECT_DIR) . '/vendor/kukrik/bootstrap-datetimepicker');
+define('QCUBED_DATETIMEPICKER_ASSETS_URL', dirname(QCUBED_BASE_URL) . '/kukrik/bootstrap-datetimepicker/assets');
+
+
+
+
